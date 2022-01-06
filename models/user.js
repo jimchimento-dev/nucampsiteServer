1b1ba00e2e70e2913d15b001462e1f1240c0b54d
Jim Chimento <chimento.dev@gmail.com>
2022-01-06 14:52:55 -0500

    Configured server to support user authentication based on FB OAuth provider.

--- models/user.js
+++ models/user.js
@@ -3,6 +3,9 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+
+    facebookId: String,
+
     firstname: {
         type: String,
         default: ''
